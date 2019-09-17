8ec0dabe2184c11f12212366737077d02c405599
rahul <rahul.kumardeveloper84@gmail.com>
2019-09-17 15:26:54 +0530

    Updates

---
+++ precache-manifest.74cf587fcbbecdf9b7378ae860ee5f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bd4de28c98c510ef3fe8618a4e2665",
+    "url": "/reactweb/index.html"
+  },
+  {
+    "revision": "f00984c6d0b41770dda5",
+    "url": "/reactweb/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a0c17eb681a3be3d0441",
+    "url": "/reactweb/static/css/main.e2c3ce2a.chunk.css"
+  },
+  {
+    "revision": "f00984c6d0b41770dda5",
+    "url": "/reactweb/static/js/2.149bb22a.chunk.js"
+  },
+  {
+    "revision": "a0c17eb681a3be3d0441",
+    "url": "/reactweb/static/js/main.cea51944.chunk.js"
+  },
+  {
+    "revision": "ef5699ec308adfce4ddf",
+    "url": "/reactweb/static/js/runtime~main.eb2ead6e.js"
+  }
+]);
